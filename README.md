2c9e22334b301e942bfc1a4faf1c6ca4f4e1da1d
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2025-11-18 00:20:18 +0100

    Loading child process

---
+++ README.md
@@ -0,0 +1,6 @@
+# SysProg opdracht Sandbox
+Dit is de code voor de SysProg opdracht over sandbox.
+
+# Build instructies
+Om dit project te bouwen wordt cmake gebruikt met de cmake extensie voor Visual Studio Code.
+
---
+++ main_src/src/main.cpp
@@ -0,0 +1,46 @@
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <windows.h>
+
+int main(int argc, char *argv[]) {
+  STARTUPINFO si;
+  PROCESS_INFORMATION pi;
+
+  ZeroMemory(&si, sizeof(si));
+  si.cb = sizeof(si);
+  ZeroMemory(&pi, sizeof(pi));
+
+  // Command line arg for CreateProcess *must* be writable
+  std::vector<char> path(1024);
+  std::strcpy(path.data(), "../other_src/other.exe");
+
+  // Start the child process.
+  if (!CreateProcess(nullptr,     // No module name (use command line)
+                     path.data(), // Command line
+                     nullptr,     // Process handle not inheritable
+                     nullptr,     // Thread handle not inheritable
+                     false,       // Set handle inheritance to FALSE
+                     0,           // No creation flags
+                     nullptr,     // Use parent's environment block
+                     nullptr,     // Use parent's starting directory
+                     &si,         // Pointer to STARTUPINFO structure
+                     &pi)         // Pointer to PROCESS_INFORMATION structure
+  ) {
+    std::cerr << "CreateProcess failed (" << GetLastError() << ")\n";
+    return 1;
+  }
+
+  // Wait until child process exits.
+  WaitForSingleObject(pi.hProcess, INFINITE);
+  DWORD status;
+  if (GetExitCodeProcess(pi.hProcess, &status) && status == 0) {
+    std::cerr << "child process exited normally\n";
+  } else {
+    std::cerr << "child process failed, error " << GetLastError() << std::endl;
+  }
+
+  // Close process and thread handles.
+  CloseHandle(pi.hProcess);
+  CloseHandle(pi.hThread);
+}
